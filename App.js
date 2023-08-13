e1578ecfc0505115f2f0c85435e08ec485205dd5
Yash Garg <112561468+YashGarg969@users.noreply.github.com>
2023-08-13 13:53:34 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,52 @@
+import logo from "./logo.svg";
+import { useContext, useEffect } from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar/index";
+import Home from "./Pages/Home";
+import axios from "./Axios";
+import { MyContext } from "./Context";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import ErrorPage from "./Pages/404/404";
+import Login from "./Pages/Login/Login";
+import Signup from "./Pages/Signup/Signup";
+import Favorites from "./Pages/Favorites/Favorites";
+
+function App() {
+  const { user, setUser } = useContext(MyContext);
+  useEffect(() => {
+    axios.post("/auto-login").then(({ data }) => setUser(data));
+  }, []);
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        {!user && (
+          <>
+            {" "}
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route exact path="/signup">
+              <Signup />
+            </Route>
+          </>
+        )}
+        {user && (
+          <Route exact path="/my-favorites">
+            <Favorites/>
+          </Route>
+        )}
+
+        <Route>
+          <ErrorPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
